2283c2c9d014fa1efc007041000892ac3a282682
Ahmet Çotur <ahmet.cotur@hotmail.com>
2022-06-05 02:15:58 +0300

    added some comments

--- host/Services/ProfilePictureAppService.cs
+++ host/Services/ProfilePictureAppService.cs
@@ -14,6 +14,9 @@ public class ProfilePictureAppService : CrudBlobAppService<ProfilePictureContain
 
     protected override Task<IRemoteStreamContent> CreateOutputStreamAsync(string name, Stream blobStream)
     {
+        // if you do not set the fileName, explorers will try to download the blob instead of displaying them.
+        // See: https://github.com/abpframework/abp/blob/a2d0169e9b7defa812c633944cab25686056c780/framework/src/Volo.Abp.AspNetCore.Mvc/Volo/Abp/AspNetCore/Mvc/ContentFormatters/RemoteStreamContentOutputFormatter.cs#L32
+        // See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Disposition#syntax
         var streamContent = new RemoteStreamContent(blobStream, null, DefaultContentType, blobStream.Length);
 
         return Task.FromResult((IRemoteStreamContent) streamContent);
--- host/Services/VideoAppService.cs
+++ host/Services/VideoAppService.cs
@@ -1,7 +1,6 @@
 ﻿using Cotur.Abp.Blob.Application;
 using Cotur.Abp.Blob.Host.BlobContainers;
 using Volo.Abp.BlobStoring;
-using Volo.Abp.Content;
 
 namespace Cotur.Abp.Blob.Host.Services;
 
@@ -10,12 +9,5 @@ public class VideoAppService : CrudBlobAppService<VideoContainer>
     public VideoAppService(IBlobContainer<VideoContainer> blobContainer) : base(blobContainer)
     {
         DefaultContentType = "video/mp4";
-    }
-    
-    protected override Task<IRemoteStreamContent> CreateOutputStreamAsync(string name, Stream blobStream)
-    {
-        var streamContent = new RemoteStreamContent(blobStream, null, DefaultContentType, blobStream.Length);
-
-        return Task.FromResult((IRemoteStreamContent) streamContent);
     }
 }
